272d0432b61af6183fdd5ae2ce6f57f10b6daee5
Wiekusvonl <vonlandsbergjohn@gmail.com>
2025-07-26 09:07:28 +0200

    Add server proxy configuration to Vite for API requests

--- altona-village-frontend/vite.config.js
+++ altona-village-frontend/vite.config.js
@@ -5,10 +5,15 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
 })
